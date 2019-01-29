358903dc1acddde577bb80be938b0c12d45fe17e
Unknown <jaxson.weaver.2022@student.lhschools.org>
2019-01-29 14:07:42 -0500

    created an intellij project

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,21 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int firstNumber, secondNumber, thirdNumber, fourthNumber, fithNumber, sixNumber;
+        String firstWord, secondWord;
+
+        firstNumber = 6;
+        secondNumber = 54;
+        thirdNumber = 9;
+        fourthNumber =
+
+
+
+
+
+
+    }
+}
